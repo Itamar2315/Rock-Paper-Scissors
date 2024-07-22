74006e054ac80230d661a2dd59f96a65703c71c0
itamar2315 <itamar2315@gmail.com>
2024-07-22 16:29:29 +0300

    add ui to the game, instead of playing through the console!

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Console rock paper scissors. 
 you should open console to play (press f12). 
 small task I did as a part of The Odin Project foundations course.
+
+Rock icon - https://www.flaticon.com/free-icons/fist Fist icons created by Cuputo - Flaticon
+
+Paper icon - "https://www.flaticon.com/free-icons/rock-paper-scissors" Rock paper scissors icons created by iconading - Flaticon
+
+scissors icon - https://www.flaticon.com/free-icons/rock-paper-scissors rock paper scissors icons Rock paper scissors icons created by Cap Cool - Flaticon
--- main.js
+++ main.js
@@ -1,6 +1,38 @@
+const scoreBoard = document.querySelector(".scoreBoard");
+const moves = document.querySelector(".moves");
+console.log("moves");
+
+let computerScore = 0;
+let playerScore = 0;
+
+const moveBoardPlayer = document.querySelector("#playerMove");
+const moveBoardComputer = document.querySelector("#computerMove");
+const scoreBoardPlayer = document.querySelector("#playerScore");
+const scoreBoardComputer = document.querySelector("#computerScore");
+const roundResult = document.querySelector("#roundResult")
+updateScore();
+
+
+moves.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+      return;
+    }
+    singleRound(event.target.id);
+});
+
+
 console.log("Hello player1");
 let roundsNum = prompt("How many rounds would you like to play?");
-playGame(roundsNum);
+
+//playGame(roundsNum);
+
+
+function updateScore(){
+    scoreBoardPlayer.textContent = "Player1 score - " + playerScore;
+    scoreBoardComputer.textContent = "Computer score - " + computerScore;
+}
+
 
 
 
@@ -39,26 +71,26 @@ function calculateWinner(playerMove, computerMove){
     //we know for sure it's not a draw so we only need to check if it's a win for the computer
     else if(playerMove === "Rock"){
         if(computerMove === "Paper"){
-            return "Computer have won this round";
+            return "Paper wraps rock, Computer have won this round";
         }
         else{
-            return "Player has won this round";
+            return "Rock breaks scissors, Player has won this round";
         }
     }
     else if(playerMove === "Paper"){
         if(computerMove === "Scissors"){
-            return "Computer have won this round";
+            return "Scissors cut paper, computer have won this round";
         }
         else{
-            return "Player has won this round";
+            return "Paper wraps rock, Player has won this round";
         }
     }
     else if(playerMove === "Scissors"){
         if(computerMove === "Rock"){
-            return "Computer have won this round";
+            return "Rock breaks scissors, Computer have won this round";
         }
         else{
-            return "Player has won this round";
+            return "Scissors cut paper, Player has won this round";
         }
     }
     else{
@@ -67,46 +99,40 @@ function calculateWinner(playerMove, computerMove){
 }
 
 
-function singleRound(){
+function singleRound(btnID){
+    
     const computerMove = getComputerChoice();
-    const playerMove = getPlayerChoice();
-    const result = calculateWinner(playerMove, computerMove);
-    console.log("The computer chose: " + computerMove);
-    console.log(result);
+    const playerMove = capitalize(btnID.replace("Btn", ""));
+    let result = calculateWinner(playerMove, computerMove);
+    moveBoardPlayer.textContent = "Player move - " + playerMove;
+    moveBoardComputer.textContent = "Computer move - " + computerMove;
+    roundResult.textContent = "Round result - " + result;
+    result = result.substring(result.indexOf(",") + 2);
     
     if(result === "Player has won this round"){
-        return "Player";
+        ++playerScore;
     }
     else if(result === "Computer have won this round"){
-        return "Computer";
-    }
-    else{
-        return "Draw"; // draw might be after player input a move that isn't rock/paper/scissors
-    }
-}
-
-function playGame(roundsNum){
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    while(playerScore != roundsNum && computerScore != roundsNum){
-        result = singleRound();
-        if(result === "Player"){
-            ++playerScore;
-        }
-        else if(result === "Computer"){
-            ++computerScore;
-        }
-        console.log("The score is- Computer:" + computerScore + " Player:" + playerScore)
-    }
-    if(playerScore > computerScore){
-        console.log("Player has won the game!")
-    }
-    else{
-        console.log("Computer have won the game!");
+        ++computerScore;
     }
 
+    if(playerScore == roundsNum){
+        alert("Player has won the game!");
+        playerScore = 0;
+        computerScore = 0;
+
+    }
+    else if(computerScore == roundsNum){
+        alert("Computer have won the game!");
+        playerScore = 0;
+        computerScore = 0;
+    }
+    updateScore();
+    
 }
+
+
+
 
 
 
